c3d634018cab5e404eaa3f8f213f87f00a512ff1
kevin-gallo <harriekevingallo@gmail.com>
2025-09-09 17:00:54 +0800

    feat: add checkout ui

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,81 @@
 'use client'
 
+import { useState, useEffect } from 'react';
+import { Home } from 'lucide-react';
+import Link from 'next/link';
+import { products } from '../data/data';
+
 const Checkout = () => {
+
+    const [cartProducts, setCartProducts] = useState<typeof products>([]);
+
+    useEffect(() => {
+        const storedCart = JSON.parse(localStorage.getItem('cart') || '[]');
+
+        if(storedCart.length !== 0) {
+            const filteredProducts = products.filter((product) => 
+                storedCart.includes(product.id)
+            );
+
+            setCartProducts(filteredProducts);
+        }
+    }, []);
+
+    console.log(cartProducts);
+
     return (
         <div>
-            <h1 className='text-5xl font-semibold text-center my-6'>Checkout Page</h1>
+            <div className='flex items-center justify-center'>
+                <div>
+                    <Link href="/" className="absolute top-4 left-4 p-2 rounded-full hover:bg-gray-300">
+                        <Home size={32} />
+                    </Link> 
+                </div>
+                <h1 className='text-5xl font-semibold text-center my-6'>Checkout</h1>
+            </div>
+            <div className='flex items-center justify-center'>
+                {
+                    cartProducts.length !== 0  ? ( 
+                        <div className='flex items-start justify-center gap-10'>
+                            <div>
+                                {
+                                    cartProducts.length !== 0 ? (
+                                        cartProducts.map((product) => (
+                                            <div key={product.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                                                <div className="md:flex">
+                                                <div className="md:flex-shrink-0">
+                                                    <img src={product.image} alt={product.name} className="h-48 w-full object-cover md:h-full md:w-48" />
+                                                </div>
+                                                <div className="p-8">
+                                                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{product.company}</div>
+                                                    <h1 className="block mt-1 text-lg leading-tight font-medium text-black">{product.name}</h1>
+                                                    <p className="mt-2 text-gray-500">{product.description}</p>
+                                                    <p className="mt-2 text-gray-900 font-bold">${product.price}</p>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        ''
+                                    )
+                                }
+                            </div>
+                            <div className='p-6 border border-gray-300 rounded'>
+                                <h2 className='text-2xl mb-2 font-semibold'>Review your cart</h2>
+                                <p>Sub-total: ${cartProducts.reduce((total, product) => total + product.price, 0)}</p>
+                                <p>Quantity: {cartProducts.length}</p>
+                                <p>Discount: 0</p>
+                                <p className='text-3xl font-bold'>
+                                    ${cartProducts.reduce((total, product) => total + product.price, 0)}
+                                </p>
+                                <button className='mt-4 text-white px-4 py-2 rounded bg-indigo-500 hover:bg-indigo-600'>Pay Now</button>
+                            </div>
+                        </div>
+                    ) : (
+                        <p className='text-center text-2xl'>No items in the cart</p>
+                    )
+                }
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,14 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [cart, setCart] = useState<number[]>([]);
-  console.log('cart', cart.length)
   const handleAddToCart = (productId: number) => {
-    console.log(`Product ${productId} added to cart`);
-
     const existingCart = JSON.parse(localStorage.getItem('cart') || '[]');
 
-    console.log(existingCart)
-
     if(!existingCart.includes(productId)) {
-      existingCart.push(productId);
-      localStorage.setItem('cart', JSON.stringify(existingCart));
+      const updatedCart = [...existingCart, productId];
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+
+      setCart(updatedCart);
     }
   };
 
@@ -32,16 +29,21 @@ export default function Home() {
       <div className='flex items-center justify-between gap-2'>
         <h1 className='text-5xl font-semibold text-center my-6'>Products</h1>
         <div>
-          <Link href={`/checkout`}>
-            <ShoppingCart className='mx-auto' size={32}/>
-            <span className='text-white text-sm bg-red-400 rounded-full p-1'>{cart.length}</span>
+          <Link href="/checkout" className="relative inline-block">
+            <ShoppingCart className="mx-auto" size={32} />
+            
+            {cart.length > 0 && (
+              <span className="absolute -top-1 -right-2 bg-red-500 text-white text-[10px] font-bold rounded-full py-[2px] px-[6px]">
+                {cart.length}
+              </span>
+            )}
           </Link>
         </div>
       </div>
-      <div className='grid grid-cols-2 gap-2'>
+      <div className='grid grid-cols-2 gap-4'>
         {(products.length !== 0 && products) ? (
           products.map((product) => (
-            <div key={product.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-4">
+            <div key={product.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
               <div className="md:flex">
                 <div className="md:flex-shrink-0">
                   <Image src={product.image} alt={product.name} width={192} height={192} className="h-auto w-auto object-cover md:h-full md:w-48" />
